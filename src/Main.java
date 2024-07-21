4d01087b88b8db7e56c21b88c87074592a65a97c
Lucas Eduardo <lesds2001@gmail.com>
2024-07-21 19:26:28 -0300

    Exibindo passo a passo do processo de ordenação

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,9 @@ import java.util.List;
 public class Main {
 
     public static void main(String[] args) {
-        int[] arr = {42, 32, 33, 52, 37, 47, 51};
+        int[] arr = {42, 75, 86, 23, 57, 68, 92, 31, 44, 10, 53, 84, 99, 36, 18, 27, 65, 12, 80, 40, 70, 91, 25, 38, 61, 47, 55, 21, 73, 59};
+        //int[] arr = {10,5 ,6 ,9 ,7, 8, 2, 1, 3, 4};
+
 
         bucketSort(arr);
 
@@ -48,12 +50,19 @@ public class Main {
             buckets.get(bucketIndex).add(num);
         }
 
+        printBuckets(buckets, "DESORDENADO");
+
         // 4) Ordenar os elementos em cada balde
         for (List<Integer> bucket : buckets) {
+
             insertionSort(bucket);
         }
 
+        printRows();
 
+        printBuckets(buckets, "ORDENADO");
+
+        printRows();
 
         // 5) Concatenar todos os baldes ordenados de volta no array original
         int index = 0;
@@ -78,6 +87,31 @@ public class Main {
             }
             bucket.set(j + 1, key);
         }
+    }
+
+    private static void printBuckets(List<List<Integer>> buckets, String msg) {
+        //Imprimindo baldes na tela (Ordenado)
+        for (int i = 0; i < buckets.size(); i++) {
+
+            System.out.print("Bucket " + msg + " " + (i + 1) + ":");
+            int index = 0;
+            for (Integer num : buckets.get(i)) {
+                index++;
+
+                if (index == buckets.get(i).size()) {
+                    System.out.print(" " + num);
+                } else {
+                    System.out.print(" " + num + " -");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    private static void printRows() {
+        System.out.println("");
+        System.out.println("// -------------------------------------------------------- //");
+        System.out.println("");
     }
 
 
